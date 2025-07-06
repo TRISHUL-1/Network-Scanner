1030813bedc4c29dd63650d5e5f575ceccc8ad96
Trishul <tbose5855@gmail.com>
2025-07-06 22:32:57 +0530

    Adding an option to scan a specific port

--- scanner.py
+++ scanner.py
@@ -50,35 +50,57 @@ banner = """
 
 print(banner)
 
-print("What do you want to do ?")
-print("1. Ping Host")
-print("2. Scan for Open Ports")
-print("3. Exit")
-option = int(input(">> "))
+while True:
+    print("What do you want to do ?")
+    print("1. Ping Host")
+    print("2. Scan for Open Ports")
+    print("3. Scan a particular port")
+    print("4. Exit")
+    option = int(input(">> "))
 
-if option == 1:
-    ip = input("Enter the ip you want to ping: ")
-    try:
-        ipaddress.ip_address(ip)
-    except ValueError:
-        print("invalid IP address")
+    if option == 1:
+        ip = input("Enter the ip you want to ping: ")
+        try:
+            ipaddress.ip_address(ip)
+        except ValueError:
+            print("invalid IP address")
+            exit(1)
+
+        ping_host(ip)
+
+    elif option == 2:
+        
+        ip = input("Enter the ip you want to ping: ")
+        try:
+            ipaddress.ip_address(ip)
+        except ValueError:
+            print("invalid IP address")
+            exit(1)
+
+        starting_range = int(input("Enter the staring value: "))
+        ending_range = int(input("Enter the ending range: "))
+
+        scan_ports_multithreading(ip, range(starting_range, ending_range + 1))
+    elif option == 3:
+
+        ip = input("Enter the ip you want to scan: ")
+        try:
+            ipaddress.ip_address(ip)
+        except ValueError:
+            print("invalid IP address")
+            exit(1)
+
+        port = int(input("Enter the port you want to scan: "))
+        if port < 1 or port > 65535:
+            print("Port number must be between 1 and 65535")
+            exit(1)
+
+        open_ports = []
+        scan_port(ip, port, open_ports)
+        if open_ports:
+            print(f"Port {port} is open on {ip}")
+        else:
+            print(f"Port {port} is closed on {ip}")
+
+    elif option == 4:
         exit(1)
-
-    ping_host(ip)
-
-elif option == 2:
-    
-    ip = input("Enter the ip you want to ping: ")
-    try:
-        ipaddress.ip_address(ip)
-    except ValueError:
-        print("invalid IP address")
-        exit(1)
-
-    starting_range = int(input("Enter the staring value: "))
-    ending_range = int(input("Enter the ending range: "))
-
-    scan_ports_multithreading(ip, range(starting_range, ending_range + 1))
-
-elif option == 3:
-    exit(1)
